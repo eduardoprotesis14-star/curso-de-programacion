ff453f19d31ff7f56ba19b80c5b58e0cd6d00f3e
eduardoprotesis14-star <eduardoprotesis14@gmail.com>
2026-03-24 16:40:53 -0400

    tarea 1

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+numero1= 20
+numero2= 30
+
+suma= numero1 + numero2
+resta= numero1 - numero2
+multiplicacion= numero1 * numero2
+division= numero1 / numero2
+
+print(f"la suma es: {suma}")
+print(f"la resta es: {resta}")
+print(f"la multiplicacion es: {multiplicacion}")
+print(f"la division es: {division}")
+
+#  ejercicio 1
+nombre = "Eduardo Leon"
+edad = 40
+print(f"hola me llamo{nombre} y tengo {edad} años")
